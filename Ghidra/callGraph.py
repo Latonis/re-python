fc61d65250e65c1e3c8d8a51378f85fe4de72ba9
Jacob Latonis <latonis@wisc.edu>
2021-12-15 15:23:21 -0600

    Better formatting in graph

--- Ghidra/callGraph.py
+++ Ghidra/callGraph.py
@@ -1,11 +1,10 @@
 import ghidra_bridge
 
 def getAddress(indx):
-	return currentProgram.getAddressFactory().getDefaultAddressSpace().getAddress(indx)
+	return currentProgram.getAddressFactory().getAddress(indx)
 
 with ghidra_bridge.GhidraBridge(namespace=globals()):
 	print(getState().getCurrentAddress().getOffset())
-	ghidra.program.model.data.DataUtilities.isUndefinedData(currentProgram, currentAddress)
 
 	listing = currentProgram.getListing()
 	functionManager = currentProgram.getFunctionManager()
@@ -23,21 +22,18 @@ with ghidra_bridge.GhidraBridge(namespace=globals()):
 		entryPoint = func.getEntryPoint()
 		name = func.getName()
 		instructions = listing.getInstructions(entryPoint, True)
-		if (name == "entry"):
-			count = 0
-			for instr in instructions:
-				address = instr.getAddress()
-				opcode = instr.getMnemonicString()
-				if opcode == "CALL" and count < 10:
-					count += 1
-					print(f"\t 0x{address}: {instr}")
-					flows = instr.getFlows()
-					if (len(flows)) == 1:
-						targetAddress = f"0x{flows[0]}"
-						graph.add_edge(name, targetAddress)
-				elif (count == 10):
-					break
 		print(f"Function: {name}, Entry Point: {entryPoint}")
+
+		for instr in instructions:
+			address = instr.getAddress()
+			opcode = instr.getMnemonicString()
+			if opcode == "CALL":
+				print(f"\t 0x{address}: {instr}")
+				flows = instr.getFlows()
+				if (len(flows)) == 1:
+					targetAddress = f"{flows[0]}" if "EXTERNAL" in str(flows[0]) else f"0x{flows[0]}"
+					targetFunc =  targetAddress if "EXTERNAL" in targetAddress else functionManager.getFunctionAt(getAddress(targetAddress)).getName()
+					graph.add_edge(name, targetFunc)
 	nx.draw(graph, with_labels=True)
 	plt.show()
 
